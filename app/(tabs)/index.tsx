b5a6889e3db47e9bf9250f110a0788ec79a337cc
rohyoonseo <ajnaval67@gmail.com>
2025-03-12 09:43:33 +0800

    tabs styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,6 @@ export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-2xl font-bold text-accent">Welcome niggasdadsa</Text>
-      <Link href="/onboarding" className="text-accent"> tite</Link>
     </View>
   );
 }
