85f469d92e5aa2a2b19f494794c602508bba2f91
rohyoonseo <ajnaval67@gmail.com>
2025-03-12 15:54:38 +0800

    search route

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,40 @@
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Text, View, Image, ScrollView } from "react-native";
+import  SearchBar  from "../components/searchBar";
 
-export default function Index() {
+import { useRouter } from "expo-router";
+
+interface Props {
+  placeholder: string;
+  onPress?: () => void;
+}
+
+export default function Index({placeholder, onPress}: Props) {
+  const router = useRouter();
+  
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-2xl font-bold text-accent">Welcome niggasdadsa</Text>
+    <View className="flex-1 bg-primary ">
+      <Image source={images.bg} className="absolute w-full z-0"/>
+
+      <ScrollView className="flex-1 px-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{
+          minHeight: '100%',
+          paddingBottom: 10,
+        }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
+
+        <View className="flex-w mt-5">
+          <SearchBar
+            onPress={() => {
+              router.push("/search");}}
+              placeholder="Search a movie"
+          />
+        </View>
+        </ScrollView> 
     </View>
   );
 }
